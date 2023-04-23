f29f6560ae6ccdc3b40854fcdcef04a55abc36f9
samkaj <kajavasamuel@gmail.com>
2023-04-23 21:31:51 +0200

    bootstrap plugin manager

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 vim.cmd([[
 augroup packer_user_config
 autocmd!
@@ -59,4 +72,8 @@ return require("packer").startup(function(use)
 	use({
 		"prettier/vim-prettier",
 	})
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
