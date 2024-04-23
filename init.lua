251ab7cd56eb27ab1ef779b1a17af36fccd0fbdc
emilsvennesson <emilsvennesson@users.noreply.github.com>
2024-04-23 18:34:01 +0200

    fix ctrl+{h|j|k|l} in file explorer

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 --* samkaj's nvim config *--
 require("plugins")
 require("settings")
+
+-- apply netrw keybindings
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "netrw",
+    callback = netrw_keybindings
+})
+
